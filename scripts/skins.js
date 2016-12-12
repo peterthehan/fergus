e4698914eb50511abec997f2877ad78511e93b9b
Johj <peter.han.9493@gmail.com>
2016-12-12 00:57:03 -0800

    Complete skin command, made mobile friendly

--- scripts/skins.js
+++ scripts/skins.js
@@ -42,7 +42,18 @@ module.exports = {
 		}
 		// 1 or more arguments
 		else {
-			if(arr[args[1]]) {
+			// list
+			if(args[1] === 'list') {
+				let t = [];
+				Object.keys(arr).forEach((key) => {
+					if(arr[key].form.length !== 0) {
+						t.push(key);
+					}
+				});
+				str += '```' + t.join(', ') + '```';
+			}
+			// <name>
+			else if(arr[args[1]]) {
 				if(arr[args[1]].form.length !== 0) {
 					str += getStats(args[1], arr);
 				}
