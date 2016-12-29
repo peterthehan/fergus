72751a2c270e70145d93d20844ac0b6f107cc588
Johj <peter.han.9493@gmail.com>
2016-12-28 23:47:06 -0800

    Fix purge command, remove watermarks from all heroes with transparency

--- commands/purge.js
+++ commands/purge.js
@@ -1,7 +1,7 @@
 exports.run = function(message, args) {
   if (message.author.id === '206161807491072000') { // limit to me
     let messageCount = 2; // by default, delete one message above
-    if (!isNaN(parseInt(args[1])) && parseInt(args[1]) > messageCount) {
+    if (!isNaN(parseInt(args[1])) && parseInt(args[1]) >= messageCount) {
       messageCount = parseInt(args[1]) + 1;
       if (messageCount > 7) {
         messageCount = 7;
