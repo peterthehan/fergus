f97af1775e3100695d5269b06a189b7e3bd8752b
Johj <peter.han.9493@gmail.com>
2017-03-09 01:25:34 -0800

    Update links

--- commands/links.js
+++ commands/links.js
@@ -1,16 +1,21 @@
 module.exports.run = (message, args) => {
   const embed = require('../util/embed.js').run()
     .setTitle('Useful Links')
-    .setDescription(
-      'Hero/SBW/Skill IRC Tier List\n' +
-      'https://goo.gl/oNQ2iF\n' +
-      'by jaetheho, Viress, sakai4eva, kamakiller\n\n' +
-      'Vyrlokar\'s Ultimate Guide to the CQ Champions\n' +
-      'https://goo.gl/M37qRm\n' +
-      'by Vyrlokar\n\n' +
-      'CQ Monuments and How To Get Them\n' +
-      'https://goo.gl/UiWxOI\n' +
-      'by /u/CalvinCopyright'
-    );
+    .addField(
+      'Tier Lists',
+      '[Hero/SBW/Skill IRC Tier List](https://goo.gl/oNQ2iF) by jaetheho, Viress, sakai4eva, kamakiller\n' +
+      '[Accurina\'s Inaccurate Tier List](https://goo.gl/bBgMTg) by Accurina')
+    .addField(
+      'Champions',
+      '[Vyrlokar\'s Ultimate Guide to the CQ Champions](https://goo.gl/M37qRm) by Vyrlokar')
+    .addField(
+      'Monuments',
+      '[CQ Monuments and How To Get Them](https://goo.gl/UiWxOI) by /u/CalvinCopyright')
+    .addField(
+      'Manacar',
+      '[Manacar Rage, Ruin, Void, and End](https://goo.gl/PbpKdG) by kamakiller')
+    .addField(
+      'Fortress of Souls',
+      '[Guide to unlocking "secret" FoS10](https://goo.gl/9BXBkD) by /u/LargeBagel');
   message.channel.sendEmbed(embed);
 };
