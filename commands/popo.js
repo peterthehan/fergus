eb189085be66a39be6e71f95cc470b20fbebb2b5
Johj <peter.han.9493@gmail.com>
2017-05-11 04:10:44 -0700

    Add popo img to cmd

--- commands/popo.js
+++ commands/popo.js
@@ -1,10 +1,13 @@
 const humanizeDuration = require('humanize-duration');
+const imagePath = require('../util/imagePath.js');
 const countInstances = require('../util/countInstances.js');
+const random = require('../util/random.js');
 const truncateString = require('../util/truncateString.js');
 const userTimer = require('../util/userTimer.js');
 
 popoInstructions = () => {
   return {
+    thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) },
     title: '!popo [go|no|me|list]',
     fields: [
       {
@@ -33,32 +36,32 @@ popoInstructions = () => {
 
 popoGo = (message) => {
   if (userTimer.exists(message.author.id)) {
-    return { title: 'Error', description: `You already have a running timer!` };
+    return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Error', description: `You already have a running timer!` };
   }
   userTimer.addTimer(message, `Popo has left!`);
-  return { title: 'Timer started', description: `I'll let you know when 15 minutes have passed!` };
+  return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Timer started', description: `I'll let you know when 15 minutes have passed!` };
 }
 
 popoNo = (id) => {
   if (userTimer.exists(id)) {
     userTimer.removeTimer(id);
-    return { title: 'Timer canceled', description: `Your timer has been canceled!` };
+    return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Timer canceled', description: `Your timer has been canceled!` };
   }
-  return { title: 'Error', description: `You don't have a timer to cancel!` };
+  return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Error', description: `You don't have a timer to cancel!` };
 }
 
 popoMe = (id) => {
   if (userTimer.exists(id)) {
-    return { title: 'Time remaining', description: remainingTimeMessage(id) };
+    return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Time remaining', description: remainingTimeMessage(id) };
   }
-  return { title: 'Error', description: `You don't have a timer to check!` };
+  return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Error', description: `You don't have a timer to check!` };
 }
 
 popoList = () => {
   const timers = userTimer.getTimers();
 
   if (Object.keys(timers).length === 0 && timers.constructor === Object) {
-    return { title: 'Error', description: 'There are no user timers to list!' };
+    return { thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) }, title: 'Error', description: 'There are no user timers to list!' };
   }
 
   const remaining = [];
@@ -71,6 +74,7 @@ popoList = () => {
 
   const description = truncateString(remaining.join('\n'), '\n');
   return {
+    thumbnail: { url: imagePath(`popo%20(${random(1, 4)})`) },
     title: `Time remaining list (${countInstances(description, '\n') + 1} of ${remaining.length})`,
     description: description
   };
