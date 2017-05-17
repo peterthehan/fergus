2aa9aa738afaa2285fc8a3b5256940c24c3968b2
Johj <peter.han.9493@gmail.com>
2017-05-17 03:05:07 -0700

    Add embed to welcome, farewell events

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,12 @@
 const config = require('../config.json');
+const embed = require('../util/embed.js');
 
 module.exports = (member) => {
-  const embed = { description: config.welcomePre + member + ' (' + member.user.tag + ')' + config.welcomePost };
-  member.guild.channels.get(member.guild.id).send({ embed: embed });
+  const e = embed.process({
+    description:
+      `${config.welcomePre} ${member} (${member.user.tag}) ${config.welcomePost}`
+  });
+
+  member.guild.channels.get(member.guild.id).send({ embed: e });
   console.log(`${member.guild.name}|${member.user.tag}: Joined the server`);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,12 @@
 const config = require('../config.json');
+const embed = require('../util/embed.js');
 
 module.exports = (member) => {
-  const embed = { description: config.farewellPre + member + ' (' + member.user.tag + ')' + config.farewellPost };
-  member.guild.channels.get(member.guild.id).send({ embed: embed });
+  const e = embed.process({
+    description:
+      `${config.farewellPre} ${member} (${member.user.tag}) ${config.farewellPost}`
+  });
+
+  member.guild.channels.get(member.guild.id).send({ embed: e });
   console.log(`${member.guild.name}|${member.user.tag}: Left the server`);
 };
