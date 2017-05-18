6db65b9ee80e7bc3293a35355131f73d2ed49796
Johj <peter.han.9493@gmail.com>
2017-05-17 17:47:25 -0700

    Fix welcome, farewell event messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,7 @@ const embed = require('../util/embed.js');
 module.exports = (member) => {
   const e = embed.process({
     description:
-      `${config.welcomePre} ${member} (${member.user.tag}) ${config.welcomePost}`
+      `${config.welcomePre}${member} (${member.user.tag})${config.welcomePost}`
   });
 
   member.guild.channels.get(member.guild.id).send({ embed: e });
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,7 +4,7 @@ const embed = require('../util/embed.js');
 module.exports = (member) => {
   const e = embed.process({
     description:
-      `${config.farewellPre} ${member} (${member.user.tag}) ${config.farewellPost}`
+      `${config.farewellPre}${member} (${member.user.tag})${config.farewellPost}`
   });
 
   member.guild.channels.get(member.guild.id).send({ embed: e });
