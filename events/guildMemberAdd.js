bb5c0b352bf21bc528f39596fd5d038e7510e6ca
Johj <peter.han.9493@gmail.com>
2017-05-27 23:59:57 -0700

    Remove welcome, farewell message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,9 +4,9 @@ const embed = require('../util/embed.js');
 module.exports = (member) => {
   const e = embed.process({
     description:
-      `${config.welcomePre}${member} (${member.user.tag})${config.welcomePost}`
+      `${config.welcomePre}${member} (${member.user.tag})${config.welcomePost}`,
   });
 
-  member.guild.channels.get(member.guild.id).send({ embed: e });
+  //member.guild.channels.get(member.guild.id).send({ embed: e });
   console.log(`${member.guild.name}|${member.user.tag}: Joined the server`);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,9 +4,9 @@ const embed = require('../util/embed.js');
 module.exports = (member) => {
   const e = embed.process({
     description:
-      `${config.farewellPre}${member} (${member.user.tag})${config.farewellPost}`
+      `${config.farewellPre}${member} (${member.user.tag})${config.farewellPost}`,
   });
 
-  member.guild.channels.get(member.guild.id).send({ embed: e });
+  //member.guild.channels.get(member.guild.id).send({ embed: e });
   console.log(`${member.guild.name}|${member.user.tag}: Left the server`);
 };
