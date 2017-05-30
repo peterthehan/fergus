0d3bf23be10f6fcdd68ce18daaaeb7f5ebfcf942
Johj <peter.han.9493@gmail.com>
2017-05-30 13:26:05 -0700

    Remove hard coding img

--- commands/fergus.js
+++ commands/fergus.js
@@ -1,11 +1,7 @@
 const embed = require('../util/embed.js');
 
 exports.run = (message, args) => {
-  const e = embed.process({
-    image: {
-      url: 'https://raw.githubusercontent.com/Johj/fergus/master/assets/fergus.png',
-    },
-  });
+  const e = embed.process({ image: { url: message.client.user.avatarURL, }, });
 
   message.channel.send({ embed: e, });
 }
