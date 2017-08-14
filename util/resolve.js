3241cb3e4c0616984d85368d4d1033474d652db5
Johj <peter.han.9493@gmail.com>
2017-08-13 21:06:04 -0700

    Update resolving to the same as cqdb

--- util/resolve.js
+++ util/resolve.js
@@ -1,24 +1,6 @@
-const data = [
-  Object.assign(...require('../Decrypted/get_textlocale_en_us_0.json')['textlocale']),
-  Object.assign(...require('../Decrypted/get_textlocale_en_us_1.json')['textlocale']),
-  Object.assign(...require('../Decrypted/get_textlocale_en_us_2.json')['textlocale']),
-  Object.assign(...require('../Decrypted/get_textlocale_en_us_3.json')['textlocale']),
-  Object.assign(...require('../Decrypted/get_textlocale_en_us_4.json')['textlocale'])
-];
-
-r = (value) => {
-  if (value !== null) {
-    for (currentData of data) {
-      if (value in currentData) {
-        return currentData[value];
-      }
-    }
-  }
-  return null;
-}
+const data = require('../Decrypted/filtered_textlocale_en_us.json');
 
 module.exports = resolve = (value) => {
-  return value !== null && value.constructor === Array
-    ? value.map(currentValue => r(currentValue))
-    : r(value);
+  //console.log(value);
+  return value in data ? data[value] : value;
 }
