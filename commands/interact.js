ac1eec812dbce7ec8bd2424fe818e32be7acc190
Johj <peter.han.9493@gmail.com>
2017-09-14 01:43:20 -0700

    Resolve edge case with bad keys from data

--- commands/interact.js
+++ commands/interact.js
@@ -42,10 +42,27 @@ interact = (index) => {
   for (i of Object.keys(filtered)) {
     let e;
 
+    // handle edge case involving bad key names in interactions data file
+    const dict = {
+      'COS_PR_6_2': 'TEXT_CHA_PR_2_6', // summer vacation maria
+      'COS_EVENT_AR_14_OLYMPIC': 'TEXT_COS_AR_14_OLYMPIC', // mamba of the national team
+      'COS_WI_5_10': 'TEXT_COS_EVENT_WI_5_WINTER', // ice queen lilith
+      'COS_AR_4_17_ROCK': 'TEXT_COS_AR_4_17_NAME', // devastating drum robin hood
+      'COS_EVENT_WI_11_OLYMPIC': 'TEXT_COS_WI_11_OLYMPIC', // samba cano
+      'COS_EVENT_WI_5_OLYMPIC': 'TEXT_COS_WI_5_OLYMPIC', // carnival lilith
+      'COS_EVENT_WA_5_LABORDAY': 'TEXT_COS_EVENT_WA_5_LAVORDAY_NAME', // monte98
+      'COS_WA_2_2_RE': 'TEXT_COS_WA_2_2', // joan, knight of roses (redesign)
+    };
+
     const flag = i.startsWith('CHA');
     const hero = flag
       ? character_visual[character_visual.findIndex(j => j.id === i)]
-      : costume[costume.findIndex(j => j.costume_name === `TEXT_${i}${j.costume_name.endsWith('_NAME') ? '_NAME' : ''}`)];
+      : costume[costume.findIndex(j => {
+          const currentSkin = i in dict
+            ? dict[i]
+            : `TEXT_${i}${j.costume_name.endsWith('_NAME') ? '_NAME' : ''}`;
+          return j.costume_name === currentSkin;
+        })];
 
     if (hero == null) {
       continue;
@@ -54,7 +71,7 @@ interact = (index) => {
     e = embed.process({
       title: resolve(flag ? hero.name : hero.costume_name),
       description: resolve(filtered[i]),
-      thumbnail: { url: imagePath(`${flag ? 'heroes' : 'skins'}/${hero.face_tex}.png`), },
+      thumbnail: { url: imagePath(`${flag ? 'heroes' : 'skins'}/${hero.face_tex}${i === 'COS_WA_2_2_RE' ? '_re' : ''}`), },
     });
 
     dialogue.push(e);
@@ -77,7 +94,7 @@ exports.run = (message, args) => {
 
       const index = !isNaN(args[0])
         && parseInt(args[0]) >= 1
-        && parseInt(args[0]) <= hero_easteregg.length - 1 // temp
+        && parseInt(args[0]) <= hero_easteregg.length
           ? parseInt(args[0])
           : random(0, hero_easteregg.length);
 
