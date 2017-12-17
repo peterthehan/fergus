4fe2257d101bdbc6a00d6f5a37f0088afce8a8b0
Johj <peter.han.9493@gmail.com>
2017-12-16 18:00:24 -0800

    Redo math, ping, and about cmds

---
+++ src/commands/about.js
@@ -0,0 +1,34 @@
+const config = require('../config.json');
+
+exports.run = (message, args) => {
+  const e = {
+    title: message.client.user.username,
+    description: `Made with ❤ by ${message.guild.members.get(config.owner_id)} (${message.guild.members.get(config.owner_id).user.tag}).` + '\n\nThis bot is not affiliated, associated, authorized by, endorsed by, or in any way officially connected with NHN Entertainment Corp. or LoadComplete Inc., or any of their subsidiaries or their affiliates.',
+    footer: { text: `Android ${config.android_version} | iOS ${config.ios_version}`, },
+    thumbnail: { url: message.client.user.avatarURL, },
+    fields: [
+      {
+        name: `Invite ${message.client.user.username}`,
+        value: '[goo.gl/nDluCQ](https://goo.gl/nDluCQ)',
+        inline: true,
+      },
+      {
+        name: 'cqdb',
+        value: '[johj.github.io/cqdb](https://goo.gl/fdg6M8)',
+        inline: true,
+      },
+      {
+        name: 'Join Servers',
+        value: '[cqdb](https://discord.gg/WjEFnzC)\n[Crusaders Quest](https://discord.gg/6TRnyhj)',
+        inline: true,
+      },
+      {
+        name: 'GitHub',
+        value: '[/Johj/fergus](https://github.com/Johj/fergus)\n[/Johj/cqdb](https://github.com/Johj/cqdb)',
+        inline: true,
+      },
+    ],
+  };
+
+  message.channel.send({ embed: e, });
+}
---
+++ src/commands/math.js
@@ -0,0 +1,33 @@
+const math = require('mathjs');
+const { getPrefix, } = require('../util/get.js');
+
+mathInstructions = (message) => {
+  const prefix = getPrefix(message);
+  const e = {
+    title: `${prefix}math [<expression>]`,
+    footer: { text: 'Visit http://mathjs.org/ for examples.', },
+    fields: [
+      {
+        name: '<expression>',
+        value: `Resolve <expression>.\n*e.g. ${prefix}math 2 + 2*`,
+      },
+    ],
+  };
+
+  return e;
+}
+
+mathInfo = (message, args) => {
+  try {
+    return { description: math.eval(args.join(' ')).toString(), };
+  } catch (error) {
+    console.log(error);
+    return { description: error.toString(), };
+  }
+}
+
+exports.run = (message, args) => {
+  const e = !args.length ? mathInstructions(message) : mathInfo(message, args);
+
+  message.channel.send({ embed: e, });
+}
