d371247acde9045a6ed65d6404593b04893963ea
Johj <peter.han.9493@gmail.com>
2017-12-16 18:25:39 -0800

    Redo fergus cmd

--- commands/fergus.js
+++
@@ -1,7 +0,0 @@
-const embed = require('../util/embed.js');
-
-exports.run = (message, args) => {
-  const e = embed.process({ image: { url: message.client.user.avatarURL, }, });
-
-  message.channel.send({ embed: e, });
-}
