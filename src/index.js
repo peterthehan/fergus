f06aaaf14fcd4c7470bae346e0f8cf12d6368cf5
Johj <peter.han.9493@gmail.com>
2017-12-16 17:38:32 -0800

    Move index and remove firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 const Discord = require('discord.js');
-const firebase = require('firebase');
 const config = require('./config.json');
-const firebaseConfig = require('./firebase.json');
-
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
 
 // create bot
 const client = new Discord.Client();
