f1c5b25ee360240c368f8ef884f7a44a9f017fb8
Johj <peter.han.9493@gmail.com>
2018-03-11 00:53:29 -0800

    Update links

--- src/commands/links.js
+++ src/commands/links.js
@@ -9,16 +9,6 @@ exports.run = (message, args) => {
         inline: true,
       },
       {
-        name: 'Manacar',
-        value: `[kamakiller](https://goo.gl/PbpKdG)\n[Comic](https://goo.gl/aJ8Yoy)`,
-        inline: true,
-      },
-      {
-        name: 'LoPF',
-        value: `[Nyaa](https://goo.gl/iqppI0)\n[Shintouyu](https://goo.gl/4i8nCb)\n[LoPF map](https://goo.gl/YtlDQH)`,
-        inline: true,
-      },
-      {
         name: 'Champions',
         value: `[Vyrlokar](https://goo.gl/M37qRm)`,
         inline: true,
@@ -29,13 +19,28 @@ exports.run = (message, args) => {
         inline: true,
       },
       {
-        name: 'Miscellaneous',
-        value: `[Corrected descs](https://goo.gl/Bz1DWB)\n[cq-assets](https://goo.gl/UzKBsq)\n[block-map](https://goo.gl/wkYdqt)`,
+        name: 'Challenge',
+        value: `[kamakiller](https://goo.gl/bkC85j)`,
+        inline: true,
+      },
+      {
+        name: 'LoPF',
+        value: `[Nyaa](https://goo.gl/iqppI0)\n[Shintouyu](https://goo.gl/4i8nCb)\n[LoPF map](https://goo.gl/YtlDQH)`,
+        inline: true,
+      },
+      {
+        name: 'Manacar',
+        value: `[kamakiller](https://goo.gl/PbpKdG)\n[Comic](https://goo.gl/aJ8Yoy)`,
         inline: true,
       },
       {
         name: 'Hasla Guides',
         value: `[Comics](https://goo.gl/HPsANc)\n[Season 2](https://goo.gl/UQdjhw)\n[Berry system](https://goo.gl/jbgmLa)`,
+        inline: true,
+      },
+      {
+        name: 'Miscellaneous',
+        value: `[cq-assets](https://goo.gl/UzKBsq)\n[block-map](https://goo.gl/wkYdqt)`,
         inline: true,
       },
     ],
